edd029e420788dc84b8f4187b43c967e03b24ede
Sebbx <56113951+Sebbx@users.noreply.github.com>
2021-05-31 19:19:45 +0200

    5 iii.

--- src/pl/lublin/wsei/java/cwiczenia/MyDB.java
+++ src/pl/lublin/wsei/java/cwiczenia/MyDB.java
@@ -1,17 +1,22 @@
 package pl.lublin.wsei.java.cwiczenia;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.Properties;
+
 public class MyDB {
 
-    String lh, dbn;
-    int p;
+    String host, dbName;
+    int port;
 
     private String user, password;
 
-    public MyDB (String localhost, int port, String dbName)
+    public MyDB (String host, int port, String dbName)
     {
-        lh = localhost;
-        p = port;
-        dbn = dbName;
+        this.host = host;
+        this.port = port;
+        this.dbName = dbName;
     }
 
     public void setUser(String newUser)
@@ -22,5 +27,33 @@ public class MyDB {
     public void setPassword(String newPassword)
     {
         this.password = newPassword;
+    }
+
+    private Connection conn = null;
+
+    private void connect() {
+        Properties connectionProps = new Properties();
+        connectionProps.put("user", user);
+        connectionProps.put("password", password);
+        connectionProps.put("serverTimezone", "Europe/Warsaw");
+
+        String jdbcString = "jdbc:mysql://" + host + ":" + port + "/" + dbName;
+
+        try {
+            conn = DriverManager.getConnection(
+                    jdbcString, connectionProps);
+        }
+        catch (SQLException e) {
+            System.out.println("Błąd podłączenia do bazy: "+jdbcString);
+            System.out.println("Komunikat błędu: "+e.getMessage());
+            conn = null;
+        }
+        System.out.println("Connected to database "+dbName);
+    }
+
+    public Connection getConnection() {
+        if (conn == null)
+            connect();
+        return  conn;
     }
 }
