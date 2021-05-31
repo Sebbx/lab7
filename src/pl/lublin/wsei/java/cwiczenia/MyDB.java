6ba2b389439487991b8f68e14074abef8709a8df
Sebbx <56113951+Sebbx@users.noreply.github.com>
2021-05-31 18:56:48 +0200

    5 ii. Zdefiniowanie dwóch prywatnych właściwości user oraz password, a następnie utworzenie ich setterów.

--- src/pl/lublin/wsei/java/cwiczenia/MyDB.java
+++ src/pl/lublin/wsei/java/cwiczenia/MyDB.java
@@ -5,10 +5,22 @@ public class MyDB {
     String lh, dbn;
     int p;
 
+    private String user, password;
+
     public MyDB (String localhost, int port, String dbName)
     {
         lh = localhost;
         p = port;
         dbn = dbName;
+    }
+
+    public void setUser(String newUser)
+    {
+        this.user = newUser;
+    }
+
+    public void setPassword(String newPassword)
+    {
+        this.password = newPassword;
     }
 }
